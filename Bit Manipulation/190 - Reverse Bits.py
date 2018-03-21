7b5d22104addab5bb475151f7b6e5822de75c4ab
JSantosha <30688015+JSantosha@users.noreply.github.com>
2018-03-21 04:19:56 -0700

    Delete 190 - Reverse Bits.py

--- Bit Manipulation/190 - Reverse Bits.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    # @param n, an integer
-    # @return an integer
-    def reverseBits(self, n):
-        t = '{:032b}'.format(n)
-        x = list(t)
-        rev = x[::-1]
-
-        return int("".join(rev), 2)
